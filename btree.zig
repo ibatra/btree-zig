3e9e360585d514e133c5b6147c045afef50d807f
ibatra <ibatra@connect.ust.hk>
2022-11-15 23:57:44 +0800

    added insertNode(WIP)

--- btree.zig
+++ btree.zig
@@ -44,8 +44,8 @@ pub fn tree(comptime degree: usize, comptime T: type) type {
             }
         }
 
-        fn deinitNode(self: *Self, node: *Node, allocator: *Allocator) void {
-            if (node.children) |children| {
+        fn deinitNode(self: *Self, n: *Node, allocator: *Allocator) void {
+            if (n.children) |children| {
                 for (children) |*child| {
                     self.deinitNode(child, allocator);
                     allocator.destroy(child);
@@ -54,15 +54,53 @@ pub fn tree(comptime degree: usize, comptime T: type) type {
             }
         }
 
-
+        pub fn insertNode(self: *Self, n: *Node, value: T) !void {
+            var i: usize = 0;
+            while (i < n.items.len and n.items[i] < value) : (i += 1) {}
+            if (i < n.items.len and n.items[i] == value) {
+                return;
+            }
+            if (n.children) |children| {
+                try self.insertNode(children[i], value);
+                return;
+            }
+            if (n.items.len < degree - 1) {
+                n.items[i] = value;
+                return;
+            }
+            // split
+            var new_node = try Node.init(self.allocator);
+            new_node.* = .{
+                .items = undefined,
+                .children = null,
+            };
+            
+            var j: usize = 0;
+            while (j < degree / 2) : (j += 1) {}
+            var k: usize = 0;
+            while (j < degree - 1) : (j += 1) {
+                new_node.items[k] = n.items[j];
+                k += 1;
+            }
+            if (i < degree / 2) {
+                n.items[i] = value;
+            } else {
+                new_node.items[i - degree / 2] = value;
+            }
+        }
     };
 }
 
  
 pub fn main() !void{
-   var allocator = std.heap.page_allocator;
-   var node1 = try node(4, u32).init(&allocator);     
-   node1.items[0] = 9;
-   std.debug.print("{}", .{node1.items[0]});
+    var allocator = std.heap.page_allocator;
+    var btree = try tree(4, u32).init(&allocator);
+    try btree.insertNode(&allocator, 1);
+    try btree.insertNode(&allocator, 2);
+    try btree.insertNode(&allocator, 3);
+//    var node1 = try node(4, u32).init(&allocator);     
+//    node1.items[0] = 9;
+//    std.debug.print("{}", .{node1.items[0]});
+
 }
 
